beee9c6546e69fc87cbfaced9c1e615237de4ba1
Ching Yan Lee <106129893+LilyChing@users.noreply.github.com>
2024-07-31 17:34:01 +0800

    Update listItem to real data

--- src/listItem.js
+++ src/listItem.js
@@ -1,184 +1,213 @@
 let ListItem =
 [
     {
-        "name": "小明",
-        "message": "生日快樂！希望你的特別日子帶給你所有應得的幸福和快樂。繼續閃耀並用你的精彩直播激勵我們！",
-        "icon": "https://example.com/avatar1.png",
-        "social_media": "@xiaoming123"
+        "time": "2024/7/7 下午 11:30:53",
+        "name": "Airey艾雷",
+        "message": "這是我認識老哥之後\n參與你的第二個生日了呢\n因為跟你的台\n才有契機開始做包含厄介調酒\n實況主調酒等等的企劃\n哥你可是我實況主印象調酒的第一款呢\n也才能認識了一群會一起出來蹬牆跳\n摸草的pepega好朋友\nREN主播生日快樂\n",
+        "icon": "https://drive.google.com/thumbnail?id=1hEZ1H8p4GJL6qhU_a0Wyz8u-slE3DRQX",
+        "gift": [],
+        "Twitter_ID": "ReCat_Airey",
+        "Twitch_ID": "艾雷",
+        "YouTube_ID": "DD調酒師艾雷",
+        "IG_ID": "recat_airey"
     },
     {
-        "name": "小紅",
-        "message": "祝你生日快樂！你的直播總是我每天的亮點。繼續保持出色，願你所有的夢想成真！",
-        "icon": "https://example.com/avatar2.png",
-        "social_media": "@xiaohong_gamer"
+        "time": "2024/7/8 下午 2:41:13",
+        "name": "雲淵",
+        "message": "R主播祝你生日快樂! 祝主播未來實況長長久久越來越好！",
+        "icon": "https://drive.google.com/thumbnail?id=1mkkPUTJbb7v6LgSnE7QtohyC6qZ_2a1j",
+        "gift": [],
+        "Twitter_ID": "yunyuan19550884",
+        "Twitch_ID": "love_07051314",
+        "YouTube_ID": "",
+        "IG_ID": ""
     },
     {
-        "name": "阿強",
-        "message": "生日快樂，最棒的主播！你的積極和能量真的很有感染力。希望你度過一個充滿愛和歡笑的美好一天！",
-        "icon": "https://example.com/avatar3.png",
-        "social_media": "@aqiang_streams"
+        "time": "2024/7/8 下午 2:50:05",
+        "name": "李梓（阿梓）",
+        "message": "祝水壩生日快樂，從塔克夫到現在的快打，不論最後輸贏，都能看到主播有努力練習，很想要把遊戲打好的那一面，主播加油。",
+        "icon": "https://drive.google.com/thumbnail?id=10wBDXmBv9L5dM_XKbHBkiN22nbKeepAM",
+        "gift": [],
+        "Twitter_ID": "jamie_lizi",
+        "Twitch_ID": "",
+        "YouTube_ID": "",
+        "IG_ID": ""
     },
     {
-        "name": "小美",
-        "message": "快樂的生日祝福！你的直播給很多人帶來了快樂。謝謝你帶來的精彩內容和快樂氛圍！",
-        "icon": "https://example.com/avatar4.png",
-        "social_media": "@xiaomei_happy"
+        "time": "2024/7/11 下午 4:24:56",
+        "name": "哈拉古羅",
+        "message": "祝主播南山同壽，福壽雙......先不鬧了，希望主播實況事業不斷往上，繼續向前邁進。",
+        "icon": "https://drive.google.com/thumbnail?id=1gyDNhhxEaPVi6q-KqAS2WMQHYJNSI52i",
+        "gift": [],
+        "Twitter_ID": "Haraguro_yakkai",
+        "Twitch_ID": "haraguro_kumashi",
+        "YouTube_ID": "atemikawase6235",
+        "IG_ID": ""
     },
     {
-        "name": "小李",
-        "message": "生日快樂！你的直播是日常生活中真正的逃避。繼續保持出色的工作，願你的生日和你一樣精彩！",
-        "icon": "https://example.com/avatar5.png",
-        "social_media": "@xiaoli_live"
+        "time": "2024/7/17 下午 11:16:09",
+        "name": "旋風SyuanFong",
+        "message": "REN主播,生日快樂~",
+        "icon": "https://drive.google.com/thumbnail?id=1U1pDoZwoSWbJnmmAniAG4l0VW03CYW7S",
+        "gift": ["https://drive.google.com/open?id=11viNuQzvwXI1kG4XTcM_PvGdr3NbSftJ"],
+        "Twitter_ID": "SyuanFong0901",
+        "Twitch_ID": "樂旋風",
+        "YouTube_ID": "SyuanFong0901",
+        "IG_ID": ""
     },
     {
-        "name": "小芳",
-        "message": "祝你生日快樂！你的創意和才華無與倫比。謝謝你與我們分享你的天賦。祝你度過一個美好的日子！",
-        "icon": "https://example.com/avatar6.png",
-        "social_media": "@xiaofang_creates"
+        "time": "2024/7/18 上午 4:17:20",
+        "name": "凜玥",
+        "message": "主播安安 我是凜玥，祝主播生日快樂啦~~\n不知不覺，成為你的觀眾一年了，在這一年裡因為進了你的DC群而認識了一群好朋友，也因為你而使我更有勇氣做一些以前不敢做的事，我真的覺得很幸運。謝謝你 Ren主播 Peeposhy",
+        "icon": "https://drive.google.com/thumbnail?id=11fPlfcGHZZl5ayDk-oxN_KEdR2ezzr8b",
+        "gift": [],
+        "Twitter_ID": "Linyue_wota",
+        "Twitch_ID": "",
+        "YouTube_ID": "",
+        "IG_ID": ""
     },
     {
-        "name": "大強",
-        "message": "生日快樂！你的直播總是讓人感到愉快。希望你的生日充滿歡笑和你最喜愛的一切！",
-        "icon": "https://example.com/avatar7.png",
-        "social_media": "@daqiang_fun"
+        "time": "2024/7/25 下午 1:58:30",
+        "name": "綠豆",
+        "message": "REN生日快樂 \n恭喜主播又老一歲\n祝你實況越開越大台\nR熙語音好甜好快樂",
+        "icon": "https://drive.google.com/thumbnail?id=1TbACA44yLRlmc0nIl_RvUvVSFRk_yTo0",
+        "gift": [],
+        "Twitter_ID": "",
+        "Twitch_ID": "illusion0324",
+        "YouTube_ID": "",
+        "IG_ID": ""
     },
     {
-        "name": "小華",
-        "message": "生日快樂！觀看你的直播總是我每週的亮點。繼續做你最擅長的事，祝你有一個美好的生日慶祝！",
-        "icon": "https://example.com/avatar8.png",
-        "social_media": "@xiaohua_plays"
+        "time": "2024/7/26 下午 5:58:28",
+        "name": "薯阿泥",
+        "message": "主播生日快樂！跟台也一年半了，除了厄介糖好好嗑OkayChamping以外，每天掛台的聲音也漸漸成為令人安心的日常。看著主播在一次次的比賽中成長，每次的竭盡全力都很令人驕傲。以後會繼續畫二創支持主播ㄉ，期待未來主播繼續閃閃發亮ヽ(´▽｀)/",
+        "icon": "https://drive.google.com/thumbnail?id=1LhuPnVLXEe5Bgs7GjQbUdPSD7jqN1o24",
+        "gift": ["https://drive.google.com/open?id=14AKkUXBTJK6xBHTZmJX2hFAC9pkW6dUA"],
+        "Twitter_ID": "lavendershuni",
+        "Twitch_ID": "",
+        "YouTube_ID": "",
+        "IG_ID": ""
     },
     {
-        "name": "阿健",
-        "message": "生日快樂！你的直播帶來了無數的快樂和娛樂。願你度過一個和你一樣精彩的一天。繼續保持出色！",
-        "icon": "https://example.com/avatar9.png",
-        "social_media": "@ajian_gamer"
+        "time": "2024/7/26 下午 11:25:32",
+        "name": "我只是一塊麵包",
+        "message": "祝帥帥&GIGACHAD的主播生日快樂！\n當然，希望主播未來順利，準備奔3 PepeLaugh\n還有，好好照顧歐虧 SoCute\n更重要的，文章區都愛你 peepoLove\n你不見得會記得我誰，但我會偷偷支持主播的 peepoUnicorn",
+        "icon": "https://drive.google.com/thumbnail?id=1w8ZaarHUn6OTE3Zqo3JR6ZD-S76bKkNa",
+        "gift": [],
+        "Twitter_ID": "breadwarning45",
+        "Twitch_ID": "ijustabread",
+        "YouTube_ID": "我只是一塊麵包",
+        "IG_ID": ""
     },
     {
-        "name": "小英",
-        "message": "送上最美好的生日祝福！你的直播是我日常生活的一部分。謝謝你帶來的快樂。享受你特別的一天！",
-        "icon": "https://example.com/avatar10.png",
-        "social_media": "@xiaoying_live"
+        "time": "2024/7/29 下午 10:16:48",
+        "name": "阿勳",
+        "message": "主播生日快樂~\n雖然最近比較少畫插圖了，但還是做了兩個動圖希望主播喜歡~",
+        "icon": "https://drive.google.com/thumbnail?id=1WmdstRjlT8oZrbvNP1zelSuScv9guQ8a",
+        "gift": ["https://drive.google.com/open?id=1R13iih8yVBUHpK5ekzA4g2dh9NAxxUSM", "https://drive.google.com/open?id=175OsEQwWZwJnaRljFEOTLoFyYvJglSfq", "https://drive.google.com/open?id=1JMlBXqbMRW24raASWcC4Cynjkqc3iops", "https://drive.google.com/open?id=1i9z-ptbBvh0TiO-Br-7PjrZ5BCqQ9Oyx"],
+        "Twitter_ID": "Ashiun1812",
+        "Twitch_ID": "",
+        "YouTube_ID": "",
+        "IG_ID": ""
     },
     {
-        "name": "大明",
-        "message": "在這個充滿希望的日子裡，願你擁有無盡的幸福和歡樂。祝你在未來的日子裡，無論遇到什麼困難，都能勇敢面對，迎刃而解。希望你每天都能擁有好運氣，健康平安，事業蒸蒸日上，家庭和睦幸福，心中的每一個願望都能實現，生活美滿快樂！",
-        "avatar": "https://example.com/avatar11.png",
-        "social_media": "@daming123"
+        "time": "2024/7/29 下午 11:40:40",
+        "name": "爽阿刺阿",
+        "message": "生日快樂 另外我想問一下主播Ren0809k的0809是什麼意思",
+        "icon": "https://drive.google.com/thumbnail?id=1QCuutM_HX6UpGg-eb4RG-k4ANHowr0a7",
+        "gift": [],
+        "Twitter_ID": "a_tsyh",
+        "Twitch_ID": "saza_tw",
+        "YouTube_ID": "saza_tw",
+        "IG_ID": ""
     },
     {
-        "name": "阿麗",
-        "message": "祝你生日快樂！希望你每天都過得開心，繼續給我們帶來更多精彩的直播內容。你真的是太棒了！",
-        "avatar": "https://example.com/avatar12.png",
-        "social_media": "@ali_happy"
+        "time": "2024/7/29 下午 11:45:45",
+        "name": "冬粉海豚",
+        "message": "主播生日快樂 追隨你也滿一年了pog，在這一年跟著你的台打屁聊天和被開媽媽笑話(X)很開心，也對你在這一年的進步感到歡喜(潤黑的頭不會在戶外台斷掉了)，祝福你未來更加順利    最後再對你說一句:生日快樂！！renkolove",
+        "icon": "https://drive.google.com/thumbnail?id=1OncnbXaTqTgQUJi5iMEmX3KZ_g1keM9p",
+        "gift": [],
+        "Twitter_ID": "",
+        "Twitch_ID": "吃冬粉的海豚 (jennyeeee)",
+        "YouTube_ID": "",
+        "IG_ID": ""
     },
     {
-        "name": "小強",
-        "message": "生日快樂！每次看你的直播都充滿了愉快和娛樂。希望你有一個美好的一天，繼續保持出色！",
-        "avatar": "https://example.com/avatar13.png",
-        "social_media": "@xiaoqiang_fun"
+        "time": "2024/7/30 上午 2:22:28",
+        "name": "待宵",
+        "message": "一年過去，雖然沒有去年那麼盛大，但希望你還喜歡我們準備的驚喜及禮物。這一年來看著你不斷突破、改變，帶給我們許多意想不到的體驗，能陪伴你走過這些回憶於有榮焉，願你在接下來的日子一切順遂，生日快樂！",
+        "icon": "https://drive.google.com/thumbnail?id=13Ay5d2JI5EJNE0sNZiY6QVW7kRtvGIEM",
+        "gift": [],
+        "Twitter_ID": "kusa_Matsuyoi",
+        "Twitch_ID": "matsuyoi66",
+        "YouTube_ID": "Matsuyoi",
+        "IG_ID": ""
     },
     {
-        "name": "阿華",
-        "message": "祝你生日快樂！你的直播總是充滿了驚喜和快樂。希望你在這個特別的日子裡，能夠擁有最美好的回憶。",
-        "avatar": "https://example.com/avatar14.png",
-        "social_media": "@ahua_streams"
+        "time": "2024/7/30 上午 11:29:26",
+        "name": "小龐",
+        "message": "哈哈老人",
+        "icon": "https://cdn.7tv.app/emote/62e94cec80419c32e6821ea1/4x.webp",
+        "gift": [],
+        "Twitter_ID": "Kuh954",
+        "Twitch_ID": "Kuh954",
+        "YouTube_ID": "Kuh954",
+        "IG_ID": ""
     },
     {
-        "name": "小琴",
-        "message": "生日快樂！你的創意和才華讓我們每天都充滿了驚喜。希望你在這個特別的日子裡，能夠實現所有的願望。",
-        "avatar": "https://example.com/avatar15.png",
-        "social_media": "@xiaoqin_creates"
+        "time": "2024/7/30 下午 12:04:00",
+        "name": "treen79home",
+        "message": "因厄介認識，因跟台了解，因風格喜愛，因態度跟隨。\n不論接下來主播決定往什麼方向，期望未來依然能共同邁進。\n就算是撈麵，也會每週去光顧的。 (. ❛ ᴗ ❛.)",
+        "icon": "https://drive.google.com/thumbnail?id=1M0mP2sJ4BmlhNwfxhF-BrciiWN7g3kUp",
+        "gift": [],
+        "Twitter_ID": "treen79home",
+        "Twitch_ID": "treen79home",
+        "YouTube_ID": "Treen79home",
+        "IG_ID": ""
     },
     {
-        "name": "阿傑",
-        "message": "在這個充滿希望的日子裡，願你擁有無盡的幸福和歡樂。祝你在未來的日子裡，無論遇到什麼困難，都能勇敢面對，迎刃而解。希望你每天都能擁有好運氣，健康平安，事業蒸蒸日上，家庭和睦幸福，心中的每一個願望都能實現，生活美滿快樂！",
-        "avatar": "https://example.com/avatar16.png",
-        "social_media": "@ajie_gamer"
+        "time": "2024/7/30 下午 5:18:13",
+        "name": "藍空",
+        "message": "主播生日快樂!!從開始看主播迄今一年多了，妳是我圖奇第一個追隨跟訂閱的實況主，在此之前我未曾接觸圖奇的實況。很喜歡主播的實況風格，希望主播也能繼續開心地做實況，帶給我們更多有趣的實況與企劃，加油!",
+        "icon": "https://drive.google.com/thumbnail?id=1QJVJXrgh4GCmak6KIH7ANEnGLDnp2zp0",
+        "gift": [],
+        "Twitter_ID": "shizukusora0829",
+        "Twitch_ID": "雫_藍空(shizuku_sora)",
+        "YouTube_ID": "",
+        "IG_ID": ""
     },
     {
-        "name": "小花",
-        "message": "生日快樂！你的直播總是充滿了快樂和驚喜。希望你在這個特別的日子裡，能夠實現所有的願望。",
-        "avatar": "https://example.com/avatar17.png",
-        "social_media": "@xiaohua_happy"
+        "time": "2024/7/30 下午 7:27:45",
+        "name": "Shiny",
+        "message": "R主播生日快樂! 起初是在YT看見精華知道主播的，之後因為廢棄之城RP和主播的直播風格非常有趣，才開始追圖奇直播，也是我第一次訂閱的主播，同時您也是我開始想剪輯精華的原因，讓原本迷茫的我找到自己的興趣以及未來想做的事，未來也會一直支持您的!",
+        "icon": "https://drive.google.com/thumbnail?id=1KhM7L_75iNm6gUknTbg7utRQEmRE3fa3",
+        "gift": [],
+        "Twitter_ID": "BrightShinyYT",
+        "Twitch_ID": "SHINYyyyyyyyyyyyyy",
+        "YouTube_ID": "ShinyClipingBright",
+        "IG_ID": ""
     },
     {
-        "name": "阿明",
-        "message": "祝你生日快樂！你的直播讓我們每天都充滿了快樂和驚喜。希望你在這個特別的日子裡，能夠擁有最美好的回憶。",
-        "avatar": "https://example.com/avatar18.png",
-        "social_media": "@aming_fun"
+        "time": "2024/7/30 下午 10:17:14",
+        "name": "林靈",
+        "message": "恭祝你福壽與天齊,祝賀你生辰快樂 peepoHappy ",
+        "icon": "https://drive.google.com/thumbnail?id=1RDPzCJo212v27GpaELDmuNVRnqieIZhL",
+        "gift": [],
+        "Twitter_ID": "",
+        "Twitch_ID": "林靈0",
+        "YouTube_ID": "",
+        "IG_ID": ""
     },
     {
-        "name": "小安",
-        "message": "生日快樂！你的創意和才華讓我們每天都充滿了驚喜。希望你在這個特別的日子裡，能夠實現所有的願望。",
-        "avatar": "https://example.com/avatar19.png",
-        "social_media": "@xiaoan_creates"
-    },
-    {
-        "name": "阿東",
-        "message": "祝你生日快樂！你的直播讓我們每天都充滿了快樂和驚喜。希望你在這個特別的日子裡，能夠擁有最美好的回憶。",
-        "avatar": "https://example.com/avatar20.png",
-        "social_media": "@adong_gamer"
-    },
-    {
-        "name": "小雪",
-        "message": "生日快樂！你的直播總是充滿了快樂和驚喜。希望你在這個特別的日子裡，能夠實現所有的願望。",
-        "avatar": "https://example.com/avatar21.png",
-        "social_media": "@xiaoxue_happy"
-    },
-    {
-        "name": "阿強",
-        "message": "在這個充滿希望的日子裡，願你擁有無盡的幸福和歡樂。祝你在未來的日子裡，無論遇到什麼困難，都能勇敢面對，迎刃而解。希望你每天都能擁有好運氣，健康平安，事業蒸蒸日上，家庭和睦幸福，心中的每一個願望都能實現，生活美滿快樂！",
-        "avatar": "https://example.com/avatar22.png",
-        "social_media": "@aqiang_fun"
-    },
-    {
-        "name": "小雨",
-        "message": "生日快樂！你的創意和才華讓我們每天都充滿了驚喜。希望你在這個特別的日子裡，能夠實現所有的願望。",
-        "avatar": "https://example.com/avatar23.png",
-        "social_media": "@xiaoyu_creates"
-    },
-    {
-        "name": "阿文",
-        "message": "祝你生日快樂！你的直播讓我們每天都充滿了快樂和驚喜。希望你在這個特別的日子裡，能夠擁有最美好的回憶。",
-        "avatar": "https://example.com/avatar24.png",
-        "social_media": "@awen_gamer"
-    },
-    {
-        "name": "小亮",
-        "message": "生日快樂！你的直播總是充滿了快樂和驚喜。希望你在這個特別的日子裡，能夠實現所有的願望。",
-        "avatar": "https://example.com/avatar25.png",
-        "social_media": "@xiaoliang_happy"
-    },
-    {
-        "name": "阿偉",
-        "message": "祝你生日快樂！你的直播讓我們每天都充滿了快樂和驚喜。希望你在這個特別的日子裡，能夠擁有最美好的回憶。",
-        "avatar": "https://example.com/avatar26.png",
-        "social_media": "@awei_fun"
-    },
-    {
-        "name": "小珊",
-        "message": "在這特別的日子裡，願你擁有幸福的笑容，健康的身體，成功的事業，和諧的家庭。希望你的每一天都充滿陽光與希望，心想事成，萬事如意，快樂無憂！",
-        "avatar": "https://example.com/avatar27.png",
-        "social_media": "@xiaoshan_creates"
-    },
-    {
-        "name": "阿強",
-        "message": "祝你生日快樂！你的直播讓我們每天都充滿了快樂和驚喜。希望你在這個特別的日子裡，能夠擁有最美好的回憶。",
-        "avatar": "https://example.com/avatar28.png",
-        "social_media": "@aqiang_fun"
-    },
-    {
-        "name": "小華",
-        "message": "生日快樂！你的直播總是充滿了快樂和驚喜。希望你在這個特別的日子裡，能夠實現所有的願望。",
-        "avatar": "https://example.com/avatar29.png",
-        "social_media": "@xiaohua_happy"
-    },
-    {
-        "name": "阿麗",
-        "message": "在這特別的日子裡，願你擁有幸福的笑容，健康的身體，成功的事業，和諧的家庭。希望你的每一天都充滿陽光與希望，心想事成，萬事如意，快樂無憂！",
-        "avatar": "https://example.com/avatar30.png",
-        "social_media": "@ali_fun"
+        "time": "2024/7/31 上午 2:24:24",
+        "name": "千千",
+        "message": "祝REN主播生日快樂~\n感謝主播的娛樂效果以及神奇月老之力，一年過去了，感恩之情依舊沒齒難忘XD\n祝福主播業配接滿滿，事業順遂賺大錢！歐虧罐罐吃到飽，稱霸頻道指日可待！",
+        "icon": "https://drive.google.com/thumbnail?id=1wxy9aJSV5Q-chPV1PGPdPbp6r9clhUJL",
+        "gift": [],
+        "Twitter_ID": "Kya3580V",
+        "Twitch_ID": "",
+        "YouTube_ID": "",
+        "IG_ID": ""
     }
 ]
 export default ListItem;
