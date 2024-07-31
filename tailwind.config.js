06c789e9a7c3ff0cdbc7611e52efaa35138b8dfa
Ching Yan Lee <106129893+LilyChing@users.noreply.github.com>
2024-07-31 16:12:15 +0800

    Update tailwind.config to add padding to the container.

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
     extend: {},
     container: {
       center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
     },
   },
   darkMode: "class",
