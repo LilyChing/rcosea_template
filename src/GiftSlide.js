73ce3808f24e680e26257456c76bfc513086b064
LilyChing <106129893+LilyChing@users.noreply.github.com>
2024-08-06 00:36:55 +0800

    Show creator info and message when hovering over slides

--- src/GiftSlide.js
+++ src/GiftSlide.js
@@ -2,14 +2,19 @@ import React from "react";
 import { useState } from "react";
 import { Image } from "@nextui-org/react"
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faTwitch, faYoutube, faInstagram, faDiscord } from '@fortawesome/free-brands-svg-icons'
 import ListItem from './listItem';
 
 function GiftSlide(props) {
   const [currentIndex, setCurrentIndex] = useState(props.index);
+  const [isHovered, setIsHovered] = useState(false);
   // console.log(props.item.gift);
 
+  // const handleMouseOver = (e) => setIsHovered(true);
+
   return (
-    <div className="flex items-center justify-center h-full relative" draggable="true" >
+    <div className="gift_container flex items-center justify-center h-full relative" onMouseOver={() => setIsHovered(true)} onMouseOut={() => setIsHovered(false)}>
       {props.item.gift.map((one_gift, index) => (
         one_gift.type == "image"?
           <Image
@@ -58,6 +63,33 @@ function GiftSlide(props) {
           </>
         : null
       ))}
+      {
+        <div className="caption backdrop-blur-sm bg-sky-700/50 p-2 lg:p-6 block lg:hidden h-fit lg:h-0">
+          <div className="flex flex-col text-center pb-0 md:pb-2 lg:pb-4">
+            <p className="text-lg break-all">{props.item.name}</p>
+            <div className="flex gap-4 justify-evenly">
+            {
+              [
+                {name: props.item.Twitter_ID, icon: faTwitter},
+                {name: props.item.Twitch_ID, icon: faTwitch}, 
+                {name: props.item.YouTube_ID, icon: faYoutube}, 
+                {name: props.item.IG_ID, icon: faInstagram},
+                {name: props.item.Discord_ID, icon: faDiscord}
+
+              ].map((social_media) => {
+                return social_media.name ?
+                <div className="flex items-center gap-1">
+                  <FontAwesomeIcon icon={social_media.icon} size="sm" />
+                  <p className="text-small break-all">{social_media.name}</p>
+                </div>
+                : null
+              })
+            }
+            </div>
+          </div>
+          <p className="whitespace-pre-line hidden md:block">「 {props.item.message} 」</p>
+        </div>
+      }
     </div>
   )
 }
